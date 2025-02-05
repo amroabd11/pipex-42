ab5d3faa6622ab0d92e21486073edc2478b91bb3
Abdelghani Amraouy <aamraouy@e2r5p10.1337.ma>
2025-02-05 10:46:33 +0100

    pipex corrected

--- Makefile
+++ Makefile
@@ -1,28 +1,22 @@
 NAME=pipex
 CC=cc
 CFLAGS= -Wall -Wextra -Werror
-SRC= error_hand.c pipex.c
-DIR=./libft
-LIBFT=$(DIR)/libft.a
+SRC= error_hand.c pipex.c\
+	libft/ft_strncmp.c libft/ft_split.c libft/ft_strjoin.c libft/ft_strlen.c libft/ft_strdup.c libft/ft_strlcpy.c
 OBJ=$(SRC:.c=.o)
 
 all: $(NAME)
 
-$(NAME): $(OBJ) $(LIBFT) libft/libft.h pipex.h
-	$(CC) $(CFLAGS) $(OBJ) $(LIBFT) -o $(NAME)
+$(NAME): $(OBJ)
+	$(CC) $(CFLAGS) $(OBJ) -o $(NAME)
 
-%.o: %.c
+%.o: %.c libft/libft.h pipex.h
 	$(CC) $(CFLAGS) -c $< -o $@
 
-$(LIBFT):
-	make -C $(DIR)
-
 clean:
-	rm -r $(OBJ)
-	make -C clean $(DIR)
+	rm -rf $(OBJ)
 
 fclean:clean
 	rm -rf $(NAME)
-	make -C fclean $(DIR)
 
 re:fclean all
--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: aamraouy <aamraouy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 09:40:35 by aamraouy          #+#    #+#             */
-/*   Updated: 2025/02/01 17:29:08 by aamraouy         ###   ########.fr       */
+/*   Updated: 2025/02/05 10:46:02 by aamraouy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,16 @@ char	*get_abs_path(char *cmd1, char **env)
 	int	i;
 
 	i = 0;
-	while (env[i] && ft_strncmp(env[i], "PATH=", 5) != 0)
+	while (env[i] && (ft_strncmp(env[i], "PATH=", 5) != 0))
 		i++;
 	env_paths = ft_split(env[i] + 5, ':');
 	i = 0;
 	while (env_paths[i])
 	{
 		abs_path = ft_strjoin(env_paths[i], "/");
-		abs_path = ft_strjoin(env_paths[i], cmd1);
-		if (access(abs_path, F_OK))
-		{//signal request to the kernel to ask for the existance of the file 
+		abs_path = ft_strjoin(abs_path, cmd1);
+		if (access(abs_path, F_OK | X_OK) == 0)
+		{
 			free_split(env_paths);
 			return (abs_path);
 		}
@@ -49,8 +49,8 @@ void	run_command(char *v, char **env)
 		exit(EXIT_FAILURE);
 	absolute_path = get_abs_path(args[0], env);
 	if (!absolute_path)
-		exit(127);//command not found
-	if (execve(absolute_path, &v, env) == -1)
+		exit(127);
+	if (execve(absolute_path, args, NULL) == -1)
 	{
 		free_split(args);
 		exit_error();
@@ -61,41 +61,45 @@ void	child_process(int *pipe_fd, char **argv, char **env)
 {
 	int	fd;
 
-	fd = open(argv[1], O_RDONLY, 0776);
+	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 		exit(1);
 	dup2(fd, 0);
+	close(fd);
 	close(pipe_fd[0]);
 	dup2(pipe_fd[1], 1);
 	run_command(argv[2], env);
 }
 
-void	parent_process(int *pipe_fd, char **argv, char **env)//here the std out of this process will be the file specified by the argv[3] // stdin will be the pipe exactly the read_end(0)
+void	parent_process(int *pipe_fd, char **argv, char **env)
 {
 	int	fd;
 
-	fd = open(argv[3], O_WRONLY, 0776);
+	fd = open(argv[4], O_CREAT | O_WRONLY | O_TRUNC , 0644);
 	if (fd == -1)
 		exit(1);
 	dup2(fd, 1);
 	close(pipe_fd[1]);
 	dup2(pipe_fd[0], 0);
-	run_command(argv[2], env);
+	run_command(argv[3], env);
 }
+
+// void f(){ system("losf -e pipex");}
 
 int	main(int argc, char **argv, char **env)
 {
+	// atexit(f);
 	int	pipe_fd[2];
 	pid_t	pr_id;
 
 	if (argc != 5)
 		exit_error();
-	// parser(argv, env);
-	pipe(pipe_fd);//here i created a buffer with two ends and this space is managed by the kernel;
-	pr_id = fork();//forked two processes;
+	pipe(pipe_fd);
+	pr_id = fork();
 	if (pr_id == -1)
-		exit(-1);
-	if (!pr_id)//this child process will handle the first process in which i execute the first command and redirect its output to the write_end of pipe;
+		return(1);
+	if (!pr_id)
 		child_process(pipe_fd, argv, env);
+	wait(NULL);
 	parent_process(pipe_fd, argv, env);
 }
